13adf6b8e36d70459116bc3031f8b0cfcb7be934
Abdessamad <abdessamadbm31@gmail.com>
2022-12-01 21:12:47 +0100

    add app configuration into config.py.

--- config.py
+++ config.py
@@ -1,13 +1,24 @@
 import os
+from flask import Flask
+
+
+#----------------------------------------------------------------------------#
+# App Config.
+#----------------------------------------------------------------------------#
+
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Enable debug mode.
 DEBUG = True
 
-# Connect to the database
+#Create the app
+app = Flask(__name__)
+app.secret_key = SECRET_KEY
 
 
 # DONE IMPLEMENT DATABASE URL
 SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:123456@localhost/fyyur'
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
